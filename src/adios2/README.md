7184dacd0b2ec5683bfa765f8d9f842cb5ddb611
Kai Germaschewski <kai.germaschewski@gmail.com>
2024-10-11 18:57:06 -0400

    add "adios2" feature

---
+++ src/adios2/README.md
@@ -0,0 +1,26 @@
+
+# Install ADIOS2 
+
+This installs ADIOS2 on an Ubuntu system
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/unh-hpc/features/adios:1": {
+        version: "2.20.1"
+    }
+}
+```
+
+## Options
+
+| Options Id | Description | Type | Default Value |
+|-----|-----|-----|-----|
+| version | ADIOS2 version | string | 2.10.1 |
+
+
+
+---
+
+_Note: This file was auto-generated from the [devcontainer-feature.json](https://github.com/unh-hpc/features/blob/main/src/color/devcontainer-feature.json).  Add additional notes to a `NOTES.md`._
---
+++ src/adios2/install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Installing ADIOS2"
+echo "The provided version is: ${VERSION}"
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+apt_get_update() {
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" >/dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+check_packages curl ca-certificates tar cmake libopenmpi-dev
+
+curl -sLo adios2.tar.gz https://github.com/ornladios/ADIOS2/archive/refs/tags/v${VERSION}.tar.gz
+
+mkdir build-adios2
+cmake -B build-adios2 -S ADIOS2-${VERSION}
+cmake --build build-adios2
+cmake --install build-adios2
+
+rm -rf adios2.tar.gz ADIOS2-${VERSION} build-adios2
